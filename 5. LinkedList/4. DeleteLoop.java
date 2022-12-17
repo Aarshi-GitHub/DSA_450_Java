b7ae971cffc31484f6eb6ff007e5664059c05790
Aarshi-GitHub <101852322+Aarshi-GitHub@users.noreply.github.com>
2022-12-17 19:18:33 +0530

    Delete Loop from Linked List using Floyd Algorithm
    Q129 DSA Sheet 450 by Love Babbar

---
+++ 5. LinkedList/4. DeleteLoop.java
@@ -0,0 +1,99 @@
+/*
+Description of Question : https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1
+Input:
+N = 3
+value[] = {1,3,4}
+X = 2
+Output: 1
+*/
+
+import java.util.*;
+
+class LinkedList{
+    Node head = null;
+    Node tail = null;
+    static class Node{
+        int data;
+        Node next;
+        Node(int val){
+            data=val;
+            next=null;
+        }
+    }
+    
+    void insertAtHead(int val){
+        if(head==null){
+           head = new Node(val);
+           tail = head;
+        } 
+        else{
+            Node temp = new Node(val);
+            temp.next=head;
+            head = temp;
+        }
+    }
+    
+    void insertAtTail(int val){
+        if(head==null){
+            insertAtHead(val);
+            return;
+        }
+        Node temp = new Node(val);
+        tail.next = temp;
+        tail = tail.next;
+    }
+    
+    void createLoop(int k){
+        if(head==null || k==0) return ;
+        Node temp = head;
+        while(k!=1){
+            temp = temp.next;
+            k--;
+        }
+        tail.next = temp;
+    }
+    
+    void removeLoop(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) break; 
+        }
+        if(slow!=fast) return;
+        slow = head;
+        while(slow!=fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        while(slow.next!=fast) slow = slow.next;
+        slow.next = null;
+    }
+    
+    boolean detectLoop(Node head){
+        if(head==null || head.next==null) return false;
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) break;
+        }
+        return slow==fast;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList l1 = new LinkedList();
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            l1.insertAtTail(sc.nextInt());
+        }
+        int k = sc.nextInt();
+        l1.createLoop(k);
+        System.out.println(l1.detectLoop(l1.head));
+        l1.removeLoop(l1.head);
+        System.out.println(l1.detectLoop(l1.head));
+    }
+}
